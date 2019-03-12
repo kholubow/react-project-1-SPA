fcaf41aa6123e78aeb44da233bb04ab5cafdff15
GCoder10 <kacper457g@gmail.com>
2019-03-12 15:03:24 +0100

    commit
    commit

--- src/components/Navigation/FooterCenterContactInfoItem/FooterCenterContactInfoItem.js
+++ src/components/Navigation/FooterCenterContactInfoItem/FooterCenterContactInfoItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import classes from './FooterCenterContactInfoItem.css';
+import worldMapImageBackground from '../../../assets/images/world-map.png';
 
 const footerCenterContactInfoItem = (props) => (
 <div className = { classes.FooterCenterContactInfoItem }>
 
+    <div className = { classes.WorldMap } >
+        <img scr = { worldMapImageBackground } />
+    </div>
+        
 </div>
 );
     
