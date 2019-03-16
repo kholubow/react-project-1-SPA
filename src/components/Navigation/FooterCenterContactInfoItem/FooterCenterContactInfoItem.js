aa370d5094c86f138083bcb1213adc2cf80e4906
GCoder10 <kacper457g@gmail.com>
2019-03-16 12:20:50 +0100

    commit
    commit

--- src/components/Navigation/FooterCenterContactInfoItem/FooterCenterContactInfoItem.js
+++ src/components/Navigation/FooterCenterContactInfoItem/FooterCenterContactInfoItem.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import classes from './FooterCenterContactInfoItem.css';
-import worldMapImageBackground from '../../../assets/images/world-map.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 const footerCenterContactInfoItem = (props) => (
 <div className = { classes.FooterCenterContactInfoItem }>
 
-    <div className = { classes.WorldMap } >
-        <img scr = { worldMapImageBackground } />
+    <p className = { classes.ContactInfoTextSettings } >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+    <div>
+        <FontAwesomeIcon icon={ faMapMarkerAlt } 
+                         className={ classes.FontAwesomeIcon } />
+                         ul. Przykład 1a/4, Olsztyn, PL    
     </div>
-        
+    <div>
+        <FontAwesomeIcon icon={ faEnvelope } 
+                         className={ classes.FontAwesomeIcon } />
+                         example@mail.com
+    </div>
+    <div>
+        <FontAwesomeIcon icon={ faMobileAlt } 
+                         className={ classes.FontAwesomeIcon } />
+                         +12 345 678 900
+    </div>
+   
 </div>
 );
     
