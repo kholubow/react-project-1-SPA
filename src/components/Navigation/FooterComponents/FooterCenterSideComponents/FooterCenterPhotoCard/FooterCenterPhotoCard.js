0ed4e322140731842cfdda5d5c29082e8ff0c8b6
GCoder10 <kacper457g@gmail.com>
2019-05-04 20:39:22 +0200

    commit
    commit

---
+++ src/components/Navigation/FooterComponents/FooterCenterSideComponents/FooterCenterPhotoCard/FooterCenterPhotoCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './FooterCenterPhotoCard.css';
+
+const footerCenterPhotoCard = (props) => (
+<div className = { classes.FooterCenterPhotoCard }>
+
+</div>
+);
+    
+
+export default footerCenterPhotoCard;
